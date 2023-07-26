2aa04f5b7043c1e287d506c791512f17972c7c49
Sankalp <104098061+sankalp-7@users.noreply.github.com>
2023-07-26 15:55:46 +0530

    Update settings.py

--- social_media_app/settings.py
+++ social_media_app/settings.py
@@ -101,10 +101,10 @@ CHANNEL_LAYERS = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'your-db-name',
-        'USER': 'user',
-        'PASSWORD': 'your-db-password',
-        'HOST': 'your-db-host',
+        'NAME': 'djinstadb',
+        'USER': 'admin',
+        'PASSWORD': 'lq11pnwifi',
+        'HOST': 'djinstadb.cct3vfiws5ig.us-east-1.rds.amazonaws.com',
         'PORT': 3306
     }
 }
