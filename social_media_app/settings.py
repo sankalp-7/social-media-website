e567763cc1195ddeedd8a11f82ab2cec5e3645e7
Sankalp <104098061+sankalp-7@users.noreply.github.com>
2023-09-27 14:17:48 +0530

    final nginx static config:DONE

--- social_media_app/settings.py
+++ social_media_app/settings.py
@@ -145,9 +145,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT=os.path.join(BASE_DIR,'static')
-
+STATIC_URL = '/static/'
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static/'),
+]
 
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
