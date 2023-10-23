31da4949469c24c4a8be1b4f034f6734d2ac21c9
Sankalp <104098061+sankalp-7@users.noreply.github.com>
2023-10-23 15:37:49 +0530

    added room data data structure to handle dynamic positioning of room with recent messages

--- Chat/views.py
+++ Chat/views.py
@@ -1,13 +1,27 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-
 from .models import Room, Message
+import collections
+import heapq
+def getlastmsg(room):
+    lastMessage=Message.objects.filter(room=room).last()
+    return lastMessage
 
 @login_required
 def rooms(request):
     rooms = Room.objects.all()
-
-    return render(request, 'Chat/rooms.html', {'rooms': rooms})
+    room_data=[]
+    for room in rooms:
+        latest_msg=getlastmsg(room)
+        room_data.append({
+            'room': room,
+            'latest_message': latest_msg,
+        })
+    
+        
+    room_data.sort(key=lambda x: x['latest_message'].date_added, reverse=True)
+    print(room_data)
+    return render(request, 'Chat/rooms.html', {'rooms': room_data})
 
 @login_required
 def room(request, slug):
@@ -15,3 +29,7 @@ def room(request, slug):
     messages = Message.objects.filter(room=room)[0:25]
 
     return render(request, 'Chat/room.html', {'room': room, 'messages': messages})
+
+def mail(request):
+    
+    return render(request, 'Chat/rooms.html')
