0527f7cc27534b4af21d2f58fef32a096808ad48
Sankalp <104098061+sankalp-7@users.noreply.github.com>
2023-10-23 15:42:14 +0530

    Update admin.py

--- post/admin.py
+++ post/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from.models import Post,Follow,Stream,Tag,Comment
+from.models import Post,Follow,Stream,Tag,Comment,LikeLogs
 admin.site.register(Tag)
 admin.site.register(Post)
 admin.site.register(Follow)
 admin.site.register(Stream)
+admin.site.register(LikeLogs)
 admin.site.register(Comment)
 # Register your models here.
