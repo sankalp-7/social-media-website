356f807e5be08d85aec5560c06f536b75eb37b3d
Sankalp <104098061+sankalp-7@users.noreply.github.com>
2023-10-23 15:35:46 +0530

    Update helper.py

--- Chat/templatetags/helper.py
+++ Chat/templatetags/helper.py
@@ -1,6 +1,7 @@
-
+import collections
 from django import template
 from djinsta.models import Profile
+from Chat.models import Message
 register = template.Library()
 
 @register.filter
@@ -12,3 +13,29 @@ def check_user(users_list):
     if users_list[0]==users_list[1]:
         return 'True'
     return 'False'
+@register.filter
+def getlastmsg(key):
+    dic=collections.defaultdict(list)
+    recent_messages=Message.objects.all()
+    for m in recent_messages:
+        val=f"{m.content}"
+        dic[m.room.slug].append(val)
+    for k,v in dic.items():
+        dic[k]=v[-1]
+    if len(dic[key])==0:
+        return ""
+    return dic[key]
+@register.filter
+def getusr(key):
+    dic=collections.defaultdict(list)
+    recent_messages=Message.objects.all()
+    for m in recent_messages:
+        val=f"{m.user}"
+        dic[m.room.slug].append(val)
+    for k,v in dic.items():
+        dic[k]=v[-1]
+    if len(dic[key])==0:
+        return ""
+    return dic[key]+":"
+
+
